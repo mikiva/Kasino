0d67c97611bb5aadef96eee19904db76d4bf5324
ndi14min <ndi14min@student.hig.se>
2015-10-21 17:13:08 +0200

    cghvjkljhgbjklökjhxfggcbjhkl

--- Kasino/src/se/hig/oodp/kasino_card_deck/Deck.java
+++ Kasino/src/se/hig/oodp/kasino_card_deck/Deck.java
@@ -4,24 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Deck {
-	
-	List<Card> deck;
-	String[] s = new String[4];
-	
-	
-	public Deck() {
-		
-		s[0] = "Clubs";
-		s[1] = "Spades";
-		s[2] = "Hearts";
-		s[3] = "Diamonds";
-		
-		deck = new ArrayList<Card>();
-	
-		for(int i = 0; i < 13; i++) {
-			
-			
-		}
-	}
 
+	List<Card> deck;
+	String[] s = {"Clubs", "Spades", "Hearts", "Diamonds"};
+
+	public Deck() {
+
+		deck = new ArrayList<Card>();
+
+		for(int i = 0; i < 4; i++) {
+			for(int n = 0; n < 13; n++) 
+				deck.add(new Card(n+1, s[i]));
+		}
+		
+	}
+	
+	public void print() { 
+		for(Card c : deck) {
+			System.out.println(c);
+		}
+		System.out.println(deck.size());
+	}
 }
