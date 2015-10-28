9592eb63dc81b10341551f2f52e6df6ac8ab34f0
ndi14min <ndi14min@student.hig.se>
2015-10-28 11:33:42 +0100

    update

--- Kasino/src/se/hig/oodp/kasino/ui/SpelPlan.java
+++ Kasino/src/se/hig/oodp/kasino/ui/SpelPlan.java
@@ -12,6 +12,7 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
@@ -49,6 +50,7 @@ public class SpelPlan extends JPanel implements MouseListener, MouseMotionListen
 
 	JLabel[] labels;
 	ArrayList<Card> playerHand;
+	ArrayList<Image> imgs;
 
 
 	int x_offset;
@@ -60,7 +62,7 @@ public class SpelPlan extends JPanel implements MouseListener, MouseMotionListen
 		iconPane = new ArrayList<JPanel>();
 		labels = new JLabel[4];
 		this.deck = deck;
-
+		imgs = new ArrayList<Image>();
 		contentPane = new JPanel();
 
 		centerPanel = new JPanel();
@@ -69,270 +71,277 @@ public class SpelPlan extends JPanel implements MouseListener, MouseMotionListen
 		userPlayerPanel = new JPanel();
 		oppositePlayerPanel = new JPanel();
 		setPreferredSize(new Dimension(1000, 800));
-		//	contentPane.setPreferredSize (new Dimension (1000, 800));
-		//centerPanel.setLayout(null);
-		//	contentPane.setLayout(new BorderLayout());
-		//Container content = new JPanel(new GridLayout(3, 3, 0, 0));
+		contentPane.setPreferredSize (new Dimension (1000, 800));
+		centerPanel.setLayout(null);
+		contentPane.setLayout(new BorderLayout());
+		Container content = new JPanel(new GridLayout(3, 3, 0, 0));
 		setLayout(null);
 		for (int i = 0; i <12; i++){
 
 
-			img = deck.getImage(i);
-			//iconPane = new JPanel(new BoxLayout(arg0, arg1));
-			label1 = new JLabel(new ImageIcon(img));
-			label1.addMouseListener(this);
-			label1.addMouseMotionListener(this);
-			//centerPanel.add(iconPane.add(label1));	
+						img = deck.getImage(i);
+						//iconPane = new JPanel(new BoxLayout(arg0, arg1));
+						label1 = new JLabel(new ImageIcon(img));
+						label1.addMouseListener(this);
+						label1.addMouseMotionListener(this);
+						//centerPanel.add(iconPane.add(label1));	
+			
+			
+//						contentPane.add(iconPane.add(label1));
+//						contentPane.add(iconPane.add(label1), BorderLayout.LINE_START);
+//						add(iconPane.add(label1), BorderLayout.WEST);
+					}
+			
+			
+			
+			
+			
+			
+			
+							for (int i = 0; i <4; i++){
+					
+								img = deck.getImage(i);
+								JLabel label1 = new JLabel(new ImageIcon(img));
+								label1.addMouseListener(this);
+								label1.addMouseMotionListener(this);
+								userPlayerPanel.add(label1);	
+					
+							}
+			
+			
+			
+			
+							for (int i = 0; i <4; i++){
+					
+								img = deck.getImage(i);
+								JLabel label1 = new JLabel(new ImageIcon(img));
+								label1.addMouseListener(this);
+								label1.addMouseMotionListener(this);
+								leftPlayerPanel.add(label1);	
+					
+							}
+							for (int i = 0; i <4; i++){
+					
+								img = deck.getImage(i);
+								JLabel label1 = new JLabel(new ImageIcon(img));
+								label1.addMouseListener(this);
+								label1.addMouseMotionListener(this);
+								oppositePlayerPanel.add(label1);	
+					
+							}
+							for (int i = 0; i <4; i++){
+					
+								img = deck.getImage(i);
+								JLabel label1 = new JLabel(new ImageIcon(img));
+								label1.addMouseListener(this);
+								label1.addMouseMotionListener(this);
+								rightPlayerPanel.add(label1);	
+					
+							}
+			
+			
+							centerPanel.setBounds(267, 155, 450, 450);
+							centerPanel.addMouseListener(this);
+							add(centerPanel);
+					
+							centerPanel.setLayout(new GridLayout(2, 6, 0, 0));
+					
+					
+							leftPlayerPanel.setBounds(-62, 93, 150, 600);
+							leftPlayerPanel.addMouseListener(this);
+							add(leftPlayerPanel);
+							leftPlayerPanel.setLayout(new GridLayout(4, 1, 0, 0));
+					
+					
+							rightPlayerPanel.setBounds(888, 80, 150, 600);
+							rightPlayerPanel.addMouseListener(this);
+							add(rightPlayerPanel);
+							rightPlayerPanel.setLayout(new GridLayout(4, 1, 0, 0));
+					
+					
+							userPlayerPanel.setBounds(267, 679, 450, 157);
+							userPlayerPanel.addMouseListener(this);
+							add(userPlayerPanel);
+							userPlayerPanel.setLayout(new GridLayout(0, 4, 0, 0));
+					
+					
+							oppositePlayerPanel.setBounds(267, -75, 450, 157);
+							oppositePlayerPanel.addMouseListener(this);
+							add(oppositePlayerPanel);
+							oppositePlayerPanel.setLayout(new GridLayout(0, 4, 0, 0));
 
 
-			//contentPane.add(iconPane.add(label1));
-			//contentPane.add(iconPane.add(label1), BorderLayout.LINE_START);
-			//add(iconPane.add(label1), BorderLayout.WEST);
+
+
+
+
+
+
+
+
+			setVisible(true);
+			add(contentPane);
+			repaint();
 		}
 
 
+		public void cardsOnTable(Player[] players){
+
+			xLoc = 100;
+			yLoc = 100;
+
+			try {
+				for (int i = 0; i < players.length; i++){
+
+					for (int j = 0; j < labels.length; j++)
+					{	
+
+						imgs.add(players[i].getHand().get(j).getImage());
+						labels[j] = new JLabel(new ImageIcon(img));
+						iconPane.add(new JPanel(new BoxLayout(labels[j], BoxLayout.X_AXIS)));
+						contentPane.add(iconPane.get(j), BorderLayout.EAST);
+						labels[i].setLocation(i+ 20, 30);
+						contentPane.add(labels[i]);
+						add(labels[i]);
+						img = deck.getImage(i);
+
+
+						xLoc += 10;
+
+					}
+				}
+			} catch (IndexOutOfBoundsException e) {
+				// TODO Auto-generated catch block
+				System.out.println("Korten slut!");
+
+				e.printStackTrace();
+			}
+
+			repaint();
+
+			//img = players[0].getHand().get(0).getImage();
+			//repaint();
+
+
+
+			//	paint(graphic);
+
+
+
+		}
+//		@Override
+//		public void paintComponent(Graphics g) {
+//			super.paintComponent(g);
+//			Graphics2D g2 = (Graphics2D) g;
+//
+//			for (int i = 0; i < imgs.size(); i++)
+//			{g2.drawImage(imgs.get(i), (i *100), 100, null);}
+//			//g2.drawLine(100, 100, 300, 300);
+//
+//
+//		}
+
+
+		@Override
+		public void mouseDragged(MouseEvent e) {
+
+
+			if (draggy != null){
+				draggy.setLocation(e.getX()-x_offset , e.getY()-y_offset);
+			}
+
+
+			System.out.println("Drag");
+
+
+
+		}
+		@Override
+		public void mouseMoved(MouseEvent e) {
+			// TODO Auto-generated method stub
+			//System.out.println("move");
 
 
 
 
+		}
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			// TODO Auto-generated method stub
+			//	System.out.println("klick");
 
-		//		for (int i = 0; i <4; i++){
-		//
-		//			img = deck.getImage(i);
-		//			JLabel label1 = new JLabel(new ImageIcon(img));
-		//			label1.addMouseListener(this);
-		//			label1.addMouseMotionListener(this);
-		//			userPlayerPanel.add(label1);	
-		//
-		//		}
+			//	System.out.println((e.getSource().toString()));
 
 
+		}
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			// TODO Auto-generated method stub
+			//System.out.println("entered");
+
+		}
+		@Override
+		public void mouseExited(MouseEvent e) {
+			// TODO Auto-generated method stub
+			//	System.out.println("exited");
 
 
-		//		for (int i = 0; i <4; i++){
-		//
-		//			img = deck.getImage(i);
-		//			JLabel label1 = new JLabel(new ImageIcon(img));
-		//			label1.addMouseListener(this);
-		//			label1.addMouseMotionListener(this);
-		//			leftPlayerPanel.add(label1);	
-		//
-		//		}
-		//		for (int i = 0; i <4; i++){
-		//
-		//			img = deck.getImage(i);
-		//			JLabel label1 = new JLabel(new ImageIcon(img));
-		//			label1.addMouseListener(this);
-		//			label1.addMouseMotionListener(this);
-		//			oppositePlayerPanel.add(label1);	
-		//
-		//		}
-		//		for (int i = 0; i <4; i++){
-		//
-		//			img = deck.getImage(i);
-		//			JLabel label1 = new JLabel(new ImageIcon(img));
-		//			label1.addMouseListener(this);
-		//			label1.addMouseMotionListener(this);
-		//			rightPlayerPanel.add(label1);	
-		//
-		//		}
+		}
+		@Override
+		public void mousePressed(MouseEvent e) {
+			// TODO Auto-generated method stub
+			System.out.println("Pressed");
+			drag=true;
 
+			JComponent jc = (JComponent)e.getSource();
+			Component child = jc.findComponentAt(e.getPoint());
+			if (child instanceof JLabel)
+			{
 
-		//		centerPanel.setBounds(267, 155, 450, 450);
-		//		//centerPanel.addMouseListener(this);
-		//		add(centerPanel);
-		//
-		//		centerPanel.setLayout(new GridLayout(2, 6, 0, 0));
-		//
-		//
-		//		leftPlayerPanel.setBounds(-62, 93, 150, 600);
-		//		//leftPlayerPanel.addMouseListener(this);
-		//		add(leftPlayerPanel);
-		//		leftPlayerPanel.setLayout(new GridLayout(4, 1, 0, 0));
-		//
-		//
-		//		rightPlayerPanel.setBounds(888, 80, 150, 600);
-		//		//rightPlayerPanel.addMouseListener(this);
-		//		add(rightPlayerPanel);
-		//		rightPlayerPanel.setLayout(new GridLayout(4, 1, 0, 0));
-		//
-		//
-		//		userPlayerPanel.setBounds(267, 679, 450, 157);
-		//		//userPlayerPanel.addMouseListener(this);
-		//		add(userPlayerPanel);
-		//		userPlayerPanel.setLayout(new GridLayout(0, 4, 0, 0));
-		//
-		//
-		//		oppositePlayerPanel.setBounds(267, -75, 450, 157);
-		//		//oppositePlayerPanel.addMouseListener(this);
-		//		add(oppositePlayerPanel);
-		//		oppositePlayerPanel.setLayout(new GridLayout(0, 4, 0, 0));
+				x_offset = e.getX() - child.getX();
+				y_offset = e.getY() - child.getY();
 
-
-
-
-
-
-
-
-
-
-		setVisible(true);
-		add(contentPane);
-		repaint();
-	}
-
-
-	public void cardsOnTable(Player[] players){
-
-		xLoc = 100;
-		yLoc = 100;
-
-		for (int i = 0; i < players.length; i++){
-
-			for (int j = 0; j < labels.length; j++)
-			{	
-
-				img = players[i].getHand().get(j).getImage();
-				labels[j] = new JLabel(new ImageIcon(img));
-				//iconPane.add(new JPanel(new BoxLayout(labels[j], BoxLayout.X_AXIS)));
-				//contentPane.add(iconPane.get(j), BorderLayout.EAST);
-			//	labels[i].setLocation(i+ 20, 30);
-				//contentPane.add(labels[i]);
-				//add(labels[i]);
-				//img = deck.getImage(i);
-				repaint();
-
-				xLoc += 0;
+				draggy = (JLabel) child;
+				draggy.setSize(draggy.getPreferredSize());
 
 			}
+
+
+
+			jc.repaint();
+
+
+
+
 		}
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			drag=false;
+
+			if (draggy != null) {
+
+				draggy.setSize(draggy.getPreferredSize());
+				draggy = null;
+			}
+
+			setLayout(null);
+			centerPanel.setLayout(null);
+			leftPlayerPanel.setLayout(null);
+			rightPlayerPanel.setLayout(null);
+			oppositePlayerPanel.setLayout(null);
+			userPlayerPanel.setLayout(null);
 
 
 
-		//img = players[0].getHand().get(0).getImage();
-		//repaint();
-
-
-
-		//	paint(graphic);
-
-
-
-	}
-	@Override
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		Graphics2D g2 = (Graphics2D) g;
-
-		
-		g2.drawImage(img, xLoc, 100, null);
-		g2.drawLine(100, 100, 300, 300);
-
-
-	}
-
-
-	@Override
-	public void mouseDragged(MouseEvent e) {
-
-
-		if (draggy != null){
-			draggy.setLocation(e.getX()-x_offset , e.getY()-y_offset);
-		}
-
-
-		System.out.println("Drag");
-
-
-
-	}
-	@Override
-	public void mouseMoved(MouseEvent e) {
-		// TODO Auto-generated method stub
-		//System.out.println("move");
-
-
-
-
-	}
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
-		//	System.out.println("klick");
-
-		//	System.out.println((e.getSource().toString()));
-
-
-	}
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-		//System.out.println("entered");
-
-	}
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-		//	System.out.println("exited");
-
-
-	}
-	@Override
-	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-		System.out.println("Pressed");
-		drag=true;
-
-		JComponent jc = (JComponent)e.getSource();
-		Component child = jc.findComponentAt(e.getPoint());
-		if (child instanceof JLabel)
-		{
-
-			x_offset = e.getX() - child.getX();
-			y_offset = e.getY() - child.getY();
-
-			draggy = (JLabel) child;
-			draggy.setSize(draggy.getPreferredSize());
+			// TODO Auto-generated method stub
 
 		}
 
 
 
-		jc.repaint();
-
 
 
 
 	}
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		drag=false;
-
-		if (draggy != null) {
-
-			draggy.setSize(draggy.getPreferredSize());
-			draggy = null;
-		}
-
-		setLayout(null);
-		centerPanel.setLayout(null);
-		leftPlayerPanel.setLayout(null);
-		rightPlayerPanel.setLayout(null);
-		oppositePlayerPanel.setLayout(null);
-		userPlayerPanel.setLayout(null);
-
-
-
-		// TODO Auto-generated method stub
-
-	}
-
-
-
-
-
-
-}
 
 
